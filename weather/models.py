ea1c5cb09c079ed705d9ec2a39e2b7649e2e4139
Stanislau Khomich <stanislaukhomich@gmail.com>
2022-02-21 20:39:43 +0300

    models

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import Permission
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UserRole(models.Model):
+    USER = 'User'
+    ADMIN = 'Admin'
+    ROLES = (
+        (USER, USER),
+        (ADMIN, ADMIN)
+    )
+    role_name = models.CharField(max_length=15, choices=ROLES, default=USER)
+
+    def __str__(self):
+        return self.role_name
+
+
+class User(AbstractUser):
+    role = models.ForeignKey(UserRole, null=True, blank=False, on_delete=models.CASCADE)
+
+
+class WedQuery(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    city = models.CharField(max_length=100)
+    temp = models.CharField(max_length=10, null=True)
+
+    def __str__(self):
+        return self.city + self.temp + str(self.date)
+
+
+class WedHistQuery(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.CharField(ax_length=100)
+    city = models.CharField(max_length=100)
+    temp = models.CharField(max_length=10, null=True)
+
+    def __str__(self):
+        return self.city + self.temp + str(self.date)
+
+
+
+class WedApi(models.Model):
+    id = models.AutoField(primary_key=True, default=None)
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- weather_aggregator/settings.py
+++ weather_aggregator/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'weather.apps.WeatherConfig',
+    'rest_framework',
+    'oauth2_provider',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
