b8556a996953744d431646cd7fdc641ea0b68329
1wq2 <stanislaukhomich@gmail.com>
2022-02-23 10:07:04 +0300

    draft stage

--- weather/models.py
+++ weather/models.py
@@ -17,12 +17,14 @@ class UserRole(models.Model):
 
 
 class User(AbstractUser):
+    name = models.CharField(max_length=100, blank=True)
     role = models.ForeignKey(UserRole, null=True, blank=False, on_delete=models.CASCADE)
+    user_api_id = models.IntegerField(default=0)
 
 
 class WedQuery(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now=True)
     city = models.CharField(max_length=100)
     temp = models.CharField(max_length=10, null=True)
 
@@ -32,7 +34,7 @@ class WedQuery(models.Model):
 
 class WedHistQuery(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.CharField(ax_length=100)
+    date = models.DateTimeField(auto_now=True)
     city = models.CharField(max_length=100)
     temp = models.CharField(max_length=10, null=True)
 
@@ -40,11 +42,16 @@ class WedHistQuery(models.Model):
         return self.city + self.temp + str(self.date)
 
 
-
 class WedApi(models.Model):
-    id = models.AutoField(primary_key=True, default=None)
+    api_id = models.AutoField(primary_key=True, default=None)
     name = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
+    querystring = models.CharField(max_length=1000)
+    headers = models.CharField(max_length=2000)
 
     def __str__(self):
         return self.name
+
+
+
+
--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from .models import WedQuery, WedHistQuery, WedApi
+import requests
+from django.shortcuts import get_object_or_404, render
+from django.core import serializers
+from django.contrib.auth import get_user_model
+from weather.utils.exceptions import ResourceNotFoundException
 
-# Create your views here.
+User = get_user_model()
+
+
+def get_weather(request, user_pk):
+    # if 'name' in request.GET:        if 'city' in querystring
+
+    # city = 'London'
+
+    user = get_object(User, user_pk)
+
+    api = WedApi(api_id=user.user_api_id)
+
+    params = {
+        'q': 'London,uk',
+        'units': 'metric',
+    }
+    response = requests.request("GET", api.url, headers=api.headers, params=params)
+    temp = response.txt['main']['temp']
+
+    wed_query = WedQuery(
+        user=user.name,
+        city='London',
+        temp=temp
+    )
+
+    wed_query.save()
+
+    return render(request, 'weather/weather.html', {'data': temp})
+
+
+def get_object(_model, pk):
+    try:
+        return _model.objects.get(pk=pk)
+    except _model.DoesNotExist:
+        raise ResourceNotFoundException
